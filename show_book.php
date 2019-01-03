47d2d52d52fe2ebeaa8b391633724b4e9efdd7ff
fr0yo1 <adrian.sandru36@yahoo.com>
2019-01-03 14:14:35 +0200

    Fixed an issue where property user_id was not checked before usage.

--- show_book.php
+++ show_book.php
@@ -13,7 +13,10 @@
 	}
 	$book = BooksManager::selectBook($book_id);
 	
-	$isAdmin = UserManager::isAdmin($_SESSION['user_id']);
+	$isAdmin = false;
+	if (isset($_SESSION['user_id'])) {
+		$isAdmin = UserManager::isAdmin($_SESSION['user_id']);
+	}
 	
 	$page_content = "book_page.php";
 	include('master_page.php');
